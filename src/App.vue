699104cc9a6afc144cc95680183fdb0cff0e5abd
AlexAlberoRibera <alberoriberaalex@gmail.com>
2026-06-02 17:21:05 +0200

    chatBot

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,17 @@ import NavBar from './components/NavBar.vue';
 import FooterApp from './components/FooterApp.vue';
 import CartDrawer from './components/CartDrawer.vue';
 import { useMovieStore } from '@/stores/movieStore';
+import '@n8n/chat/style.css';
+import { createChat } from '@n8n/chat';
 
 const store = useMovieStore();
 
 onMounted(async () => {
   await store.restoreSession();
   await store.fetchMovies();
+  createChat({
+		webhookUrl: 'http://localhost:5678/webhook/989f083e-2985-4b38-bea9-a48434910ecb/chat'
+	});
 });
 </script>
 
